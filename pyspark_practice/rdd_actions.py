a190ace4d367a8d551fa6aa040e831e77b7ccbfe
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-21 21:11:38 +0530

    rdd transformations and actions

--- pyspark_practice/rdd_actions.py
+++ pyspark_practice/rdd_actions.py
@@ -4,22 +4,6 @@ sc = SparkContext("local", "RDD actions example")
 # Create an RDD
 rdd = sc.parallelize([1, 2, 3, 4, 5])
 
-# collect action
-collected_rdd = rdd.collect()
-print("Collected RDD:", collected_rdd)
-
-# take action
-taken_rdd = rdd.take(3)
-print("Taken RDD:", taken_rdd)
-
-# top action
-top_rdd = rdd.top(3)
-print("Top RDD:", top_rdd)
-
-# count action
-count_rdd = rdd.count()
-print("Count RDD:", count_rdd)
-
 # min action
 min_rdd = rdd.min()
 print("Min RDD:", min_rdd)
