37b47405517c3a273496524e2ace631c6a64169e
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-03-22 18:33:56 +0530

    rdd actions

--- pyspark/rdd/rdd_actions.py
+++ pyspark/rdd/rdd_actions.py
@@ -16,3 +16,40 @@ print("Max RDD:", max_rdd)
 mean_rdd = rdd.mean()
 print("Mean RDD:", mean_rdd)
 
+# collect action
+all_elements = rdd.collect()
+print("All Elements:", all_elements)
+
+# take action
+first_three_elements = rdd.take(3)
+print("First Three Elements:", first_three_elements)
+
+# top action
+top_three_elements = rdd.top(3)
+print("Top Three Elements:", top_three_elements)
+
+# reduce action
+from operator import add
+sum_of_elements = rdd.reduce(add)
+print("Sum of Elements:", sum_of_elements)
+
+# countByKey action
+pair_rdd = sc.parallelize([(1, 'a'), (2, 'b'), (1, 'c'), (2, 'd'), (3, 'e')])
+count_by_key = pair_rdd.countByKey()
+print("Count by Key:", count_by_key)
+
+# countByValue action
+count_by_value = rdd.countByValue()
+print("Count by Value:", count_by_value)
+
+# fold action
+initial_value = 0
+sum_fold = rdd.fold(initial_value, add)
+print("Sum using fold action:", sum_fold)
+
+# range action
+rdd_range = sc.range(1, 10, 2)
+print("Range RDD:", rdd_range.collect())
+
+
+
