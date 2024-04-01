bc0d4be9db50e898e47ac72d08cf28215b90fe60
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-04-01 20:43:22 +0530

    reading csv  file

--- pyspark/dataframe/schema/read_csv.py
+++ pyspark/dataframe/schema/read_csv.py
@@ -1,9 +1,14 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName("read csv").getOrCreate()
-flight_df = spark.read.format("csv")\
-    .option("header","false")\
-    .option("inferschema","false")\
-    .option("mode","FAILFAST")\
-    .load("2010-summary.csv")
+from pyspark.sql import *
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType , DateType
+spark = SparkSession.builder.appName('Manvi').getOrCreate()
+schema=StructType([
+    StructField("Dest_countryname",StringType()),
+    StructField("origin_countryname", StringType()),
+    StructField("count", IntegerType()),
+])
 
-
+#using custom schema reading a csv file
+custom_schema_df = spark.read.csv(path=r'C:\Users\ManviKhandelwal\Downloads\2010-summary.csv' ,
+                                  schema=schema , header=True)
+custom_schema_df.show()
+custom_schema_df.printSchema()
