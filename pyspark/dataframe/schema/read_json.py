0492664e020ab4b3670e3caa649baf1555092ca2
Manvi25 <manvi.khandelwal@diggibyte.com>
2024-04-01 20:43:35 +0530

    reading json file

--- pyspark/dataframe/schema/read_json.py
+++ pyspark/dataframe/schema/read_json.py
@@ -1,6 +1,6 @@
 from pyspark.sql import *
 
 spark = SparkSession.builder.appName('Manvi').getOrCreate()
-df = spark.read.option("multiline", "true").json("sample4.json")
+df = spark.read.option("multiline", "true").json(r"C:\Users\ManviKhandelwal\Downloads\practice.json")
 df.show(truncate=False)
 df.printSchema()
